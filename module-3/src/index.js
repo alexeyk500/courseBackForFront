2319c4c6dc43587bcca4d88f6b8e975e689dbf20
alexeyk500 <alexeyk500@yandex.ru>
2025-05-26 12:19:25 +0300

    del

---
+++ module-3/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum_1 = require("./utils/sum");
+console.log(process.env.MY_SECRET_KEY);
+console.log((0, sum_1.sum)(2, 3));
+console.log(__dirname);
