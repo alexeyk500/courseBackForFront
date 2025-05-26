840173dfac3bcf7172275510db10410dbb084244
alexeyk500 <alexeyk500@yandex.ru>
2025-05-26 17:14:37 +0300

    customMiddleware - done

---
+++ module-4-ToDoRouter/src/middlewares/hasValidId.ts
@@ -0,0 +1,8 @@
+import {Request, Response, NextFunction} from "express";
+
+export const hasValidId = (req: Request, res: Response, next: NextFunction) => {
+  if (Number.isNaN(+req.params.id)) {
+    res.status(400).json({message: 'Incorrect id'})
+  }
+  next();
+}
--- module-4-ToDoRouter/src/routes/todos.ts
+++ module-4-ToDoRouter/src/routes/todos.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import { createTodo, deleteTodo, getAllTodos, getTodoById, updateTodo } from "../controllers/todos";
+import { hasValidId } from "../middlewares/hasValidId";
 
 const todosRouter = Router();
 
 todosRouter.get('/', getAllTodos);
 
-todosRouter.get('/:id', getTodoById);
+todosRouter.get('/:id', [hasValidId], getTodoById);
 
 todosRouter.post('/', createTodo);
 
-todosRouter.put('/:id', updateTodo)
+todosRouter.put('/:id', [hasValidId], updateTodo)
 
-todosRouter.delete('/:id', deleteTodo)
+todosRouter.delete('/:id', [hasValidId], deleteTodo)
 
 export default todosRouter
