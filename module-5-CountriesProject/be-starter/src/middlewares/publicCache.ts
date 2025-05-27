cb974245df1e4333727fa4b654176f853b86edb7
alexeyk500 <alexeyk500@yandex.ru>
2025-05-27 17:38:50 +0300

    browserCaches - done

---
+++ module-5-CountriesProject/be-starter/src/middlewares/publicCache.ts
@@ -0,0 +1,7 @@
+import {Request, Response, NextFunction} from 'express'
+const publicCache = (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  next();
+}
+
+export default publicCache;
--- module-5-CountriesProject/be-starter/src/routers/countries.ts
+++ module-5-CountriesProject/be-starter/src/routers/countries.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import { getAllCountries, getCountryByName } from "../controllers/countries";
+import publicCache from '../middlewares/publicCache';
 
 const countriesRouter = Router();
 
-countriesRouter.get('/', getAllCountries);
-countriesRouter.get('/name/:name', getCountryByName);
+countriesRouter.get('/', [publicCache], getAllCountries);
+countriesRouter.get('/name/:name', [publicCache], getCountryByName);
 
 export default countriesRouter;
