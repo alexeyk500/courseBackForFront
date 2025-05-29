7df5ae0e586bb5ed804d58706565fefb690ea46d
alexeyk500 <alexeyk500@yandex.ru>
2025-05-29 10:03:12 +0300

    mongoConnected - done

--- module-7-MongoDB/src/app.ts
+++ module-7-MongoDB/src/app.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express();
+const { PORT, MONGO_URL } = process.env;
 
-app.listen(3000, () => {
-  console.log('Server started on 3000');
-});
+const run = async () => {
+  try {
+    await mongoose.connect(MONGO_URL as string);
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log('Server started on', PORT);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+run().then();
