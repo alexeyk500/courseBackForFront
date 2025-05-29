af0f8f9029a9cbae5df6fb0b6ee248056b96b8cc
alexeyk500 <alexeyk500@yandex.ru>
2025-05-29 10:31:47 +0300

    userModel - done

---
+++ module-7-MongoDB/src/models/user.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = model('user', UserSchema);
+
+export default User;
